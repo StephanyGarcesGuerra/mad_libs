6b253b6625145130f7bd53c42f29e11316af84da
StephanyGarcesGuerra <stephanygarces96@gmail.com>
2025-03-31 14:44:40 -0400

    made changes to the math/numbers

--- madLibs.js
+++ madLibs.js
@@ -3,7 +3,7 @@ let building_type = "residential building"
 let adverb = "stinky"
 let access_type = "Denied"
 let booleanValue = access_type.includes("Granted")
-let numberOne = 5
+let numberOne = 49
 let numberTwo = 7
 let mathResults = numberOne/numberTwo
 
@@ -14,7 +14,7 @@ let person_object ={
 };
 pick_name = person_object.name
 
-const story = `In a hidden ${building_type}, I stumbled upon a hidden robot factory. The ${adverb} machines were assembling gadgets endlessly. One robot, named Robo${pick_name}, handed me a metallic keycard. It beeped and said, 'Access Granted!' Did you know ${numberOne} ÷ ${numberTwo} equals ${person_object.dogs_owned}? Fascinating, right?`
+const story = `In a hidden ${building_type}, I stumbled upon a hidden robot factory. The ${adverb} machines were assembling gadgets endlessly. One robot, named Robo${person_object.name}, handed me a metallic keycard. It beeped and said, 'Access Granted!' Did you know ${numberOne} ÷ ${numberTwo} equals ${mathResults}? Fascinating, right?`
 
 document.getElementById("madlibs-output").textContent = story;
 
